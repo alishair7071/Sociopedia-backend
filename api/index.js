ad804f92a38bb149625ada7529590ce488458ad4
Ali_Shair <alishair7071@gmail.com>
2025-04-17 14:57:44 +0500

    remove / from the front end link in cors

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
 app.use(cors({
-  origin: "https://sociopedia-front-end.netlify.app/",
+  origin: "https://sociopedia-front-end.netlify.app",
   credentials: true,
 }));
 app.use(express.json());
