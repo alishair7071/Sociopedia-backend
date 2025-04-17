bc8275a137619067fc1052310b869aeadebb1360
Ali_Shair <alishair7071@gmail.com>
2025-04-17 15:30:27 +0500

    use cors

--- api/index.js
+++ api/index.js
@@ -23,10 +23,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
-app.use(cors({
-  origin: "https://sociopedia-front-end.netlify.app",
-  credentials: true,
-}));
+app.use(cors());
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
